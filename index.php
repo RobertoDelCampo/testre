e6a535939507fa5a176bea56b0728d3680f1948a
RobertoDelCampo <robertogdelc@gmail.com>
2023-06-21 16:37:12 -0600

    asd

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+namespace MiNameSpace{
+    class Hola {
+        public function __construct(){
+            echo "Hola mundo :)";
+            return;
+        }
+    }
+}
+
+
+
+$v = new Hola();
