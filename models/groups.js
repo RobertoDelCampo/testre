2ead6864d671a38615393614c5bd895f0a5dbc0c
Leonardo Cruz <leocruzpersonal@gmail.com>
2023-06-29 17:21:02 -0600

    Modelos con "sequelize model:generate"

---
+++ models/groups.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Groups extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Groups.init({
+    id_group: DataTypes.INTEGER,
+    name_group: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Groups',
+  });
+  return Groups;
+};
