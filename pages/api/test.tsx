4b08e09cf564db760fb296199b3e12b24688a4d8
RobertoDelCampo <robertogdelc@gmail.com>
2023-07-03 16:14:42 -0600

    update

--- pages/api/test.tsx
+++ pages/api/test.tsx
@@ -27,7 +27,7 @@ export default function handler(req, res) {
       const { name, email } = req.body;
       const newUser = { id: users.length + 1, name, email };
       users.push(newUser);
-      res.status(201).json(newUser);
+      res.status(201).json(users);
       break;
 
     case 'PUT':
