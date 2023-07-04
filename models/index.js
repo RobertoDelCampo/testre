34c08340f4293450fff08431429fe119950667ff
Roberto del Campo <robertogdelc@gmail.com>
2023-07-03 19:35:09 -0600

    Up

--- models/index.js
+++ models/index.js
@@ -4,26 +4,24 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
-const basename = path.basename(__filename);
+const appRoot = require('app-root-path');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(path.join(appRoot.path, '/config/config.json'))[env];
 const db = {};
-const modelsDirectory = path.resolve(__dirname, '../models');
+const modelsDirectory = path.join(appRoot.path, '/models');
 
 let sequelize;
 sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-console.log(`Dirname: ${__dirname}`)
-
-const Users = require('./users')(sequelize, Sequelize.DataTypes);
-db[Users.name] = Users;
+console.log(`App root: ${appRoot.path}`)
+console.log(`Models directory: ${modelsDirectory}`)
 
 fs
   .readdirSync(modelsDirectory)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
-      file !== basename &&
+      file !== 'index.js' &&  // Exclude the index.js file
       file.slice(-3) === '.js' &&
       file.indexOf('.test.js') === -1
     );
